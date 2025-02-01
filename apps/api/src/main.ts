e5136f6cd0e5d90948f9c75eb23b27bd3d09cfd2
Timo Heman <timoheman16@gmail.com>
2025-02-01 15:01:30 +0100

    add sign up api route

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 8000);
+    const app = await NestFactory.create(AppModule)
+    await app.listen(process.env.PORT ?? 8000)
+
+    // Apply global validation pipe to all controllers
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+        }),
+    )
 }
-bootstrap();
+
+bootstrap()
