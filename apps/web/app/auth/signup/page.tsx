ea534912049d6417e1a346df4fd16a3f93ef672e
Timo Heman <timoheman16@gmail.com>
2025-02-01 16:00:59 +0100

    add shadcn ui

--- apps/web/app/auth/signup/page.tsx
+++ apps/web/app/auth/signup/page.tsx
@@ -1,7 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
 
 function SignupPage() {
-    return <div>SignupPage</div>
+    return (
+        <div className="bg-white p-8 rounded-lg shadow-lg w-96 flex flex-col justify-center items-center">
+            <h1 className="text-center text-2xl font-bold mb-4">Sign Up</h1>
+
+            {/* Sign Up Form */}
+
+            <div className="flex justify-between text-sm">
+                <p>Already have an account? </p>
+                <Link className="underline ml-1" href="/auth/signin">
+                    Sign In
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 export default SignupPage
