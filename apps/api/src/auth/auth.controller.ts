04b7cc459107b7b53d517eb153717fadb7d67d48
Timo Heman <timoheman16@gmail.com>
2025-02-02 07:35:11 +0100

    test jwt strategy in frontend

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -23,6 +23,8 @@ export class AuthController {
     @UseGuards(JwtAuthGuard)
     @Get('protected')
     getAll(@Request() req) {
-        return `This is a protected resource; You user id is ${req.user.id}`
+        return {
+            message: `This is a protected resource; You user id is ${req.user.id}`,
+        }
     }
 }
--- apps/web/app/profile/page.tsx
+++ apps/web/app/profile/page.tsx
@@ -1,5 +1,14 @@
-function ProfilePage() {
-    return <div>ProfilePage</div>
+import { getProfile } from '@/lib/actions/profile'
+
+async function ProfilePage() {
+    const data = await getProfile()
+
+    return (
+        <div>
+            <h1>Profile page</h1>
+            <p>{data?.message}</p>
+        </div>
+    )
 }
 
 export default ProfilePage
